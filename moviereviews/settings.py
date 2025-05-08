e419009b03bb24c806d0741d4711953867bc166f
CamiloSAcevedo <142268173+CamiloSAcevedo@users.noreply.github.com>
2025-05-08 10:17:18 -0500

    Update Allowed_Host

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-51k@6%zq(a_5nudbvm5)8xlgx8-+lor)nci^rpd#dz+k5_vsyu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [18.233.170.89]
 
 
 # Application definition
